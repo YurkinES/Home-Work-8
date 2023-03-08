97cbfdb87c86ed9e700ec9a2283754af0c45431a
YurkinES <6eJlomop@inbox.ru>
2023-03-09 01:01:00 +0300

    4 stage

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //Task #1
         int[] arrT1 = new int[] {1, 2, 3};
-        double[] arrT2 = new double[] {1.57, 7.654, 9.986};
+        double[] arrT2 = {1.57, 7.654, 9.986};
         int[] arrT3 = {35, 24, 346, 25, 678, 6546, 323, 68, 47, 567, 976, 6};
         //Task #2
         for(int index = 0; index < arrT1.length; index++ ) {
