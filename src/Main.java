cfa3244df403259d7ca35124d9cc48515f4aa91f
YurkinES <6eJlomop@inbox.ru>
2023-03-09 00:06:59 +0300

    1 stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task #1
+        int[] arrT1 = new int[] {1, 2, 3};
+        double[] arrT2 = new double[] {1.57, 7.654, 9.986};
+        int[] arrT3 = {35, 24, 346, 25, 678, 6546, 323, 68, 47, 567, 976, 6};
     }
 }
